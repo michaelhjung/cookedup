576aa119a3d445fe008ea92ef7c158ccf2c5ff35
Michael Jung <jung.michaelh@gmail.com>
2024-04-05 21:17:10 -0700

    Add logo and favicon

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,11 +1,12 @@
-import Icon from "@/components/icon";
+import Image from "next/image";
+import chefBulb from "@public/imgs/chef-bulb.png";
 
 const Heading = () => (
   <section className="flex flex-col justify-center items-center">
+    <Image src={chefBulb} alt="chef lightbulb" className="w-8 md:w-12 xl:w-16" />
     <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
       <span className="font-bold text-pastel-green">cooked</span>
       <span className="text-pastel-brown">up</span>
-      <Icon type="chefhat" />
     </h1>
     <p className="text-xs sm:text-sm md:text-base">Find recipes based on ingredients on hand!</p>
   </section>
