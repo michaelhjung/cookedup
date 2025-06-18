1d7f7d5656d85fef4d29ecd0a620ed377f999bfd
Michael Jung <jung.michaelh@gmail.com>
2025-06-17 20:56:55 -0700

    Update footer copyright

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className="mt-4 flex flex-col items-center justify-center backdrop-blur-md">
       <div className="flex flex-col items-center text-xs text-slate-800 sm:text-sm">
         <p className="flex items-center gap-1 whitespace-nowrap">
-          <span>Designed for you with</span>
+          <span>Built with</span>
           <Icon
             type="heart"
             className={heartClasses}
@@ -25,7 +25,7 @@ const Footer = () => {
           </a>
         </p>
         <span className="mt-1 text-[0.6rem] opacity-50 sm:text-xs">
-          Copyright &copy; 2024 Michael Jung. All rights reserved.
+          Copyright &copy; 2024-2025 Michael Jung. All rights reserved.
         </span>
       </div>
     </footer>
