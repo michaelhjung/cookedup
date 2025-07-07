a3ab58362ed6ea6ba71b54d6ebcfa6ce336554ca
Michael Jung <jung.michaelh@gmail.com>
2025-07-06 22:35:33 -0700

    Capitalize component file names

---
+++ src/components/loaders/Bowl.tsx
@@ -0,0 +1,16 @@
+import Icon from "@components/Icon";
+
+const Bowl = () => (
+  <>
+    <Icon
+      className="-mb-6 toss-veggies text-4xl sm:-mb-10 sm:text-6xl md:-mb-12 md:text-7xl lg:-mb-16 lg:text-8xl"
+      type="veggies"
+    />
+    <Icon
+      className="toss-bowl text-4xl text-pastel-brown sm:text-6xl md:text-7xl lg:text-8xl"
+      type="bowl"
+    />
+  </>
+);
+
+export default Bowl;
