ab31f94ed945fc7e891b8f16141d4caaa0aaa8a5
Michael Jung <jung.michaelh@gmail.com>
2025-12-08 10:45:57 -0800

    Fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextTypescript from "eslint-config-next/typescript";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import pluginImport from "eslint-plugin-import";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
+  ...nextTypescript,
   eslintConfigPrettier,
-
   {
     files: ["src/**/*.{js,jsx,ts,tsx}"],
     plugins: {
@@ -112,6 +101,15 @@ const eslintConfig = [
         },
       },
     },
+  },
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
   },
 ];
 
