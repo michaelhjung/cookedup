957a47ecbd15b19c7262bbea1483416e324b2451
Michael Jung <jung.michaelh@gmail.com>
2025-12-08 10:51:43 -0800

    Update ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import pluginImport from "eslint-plugin-import";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const eslintConfig = [
   ...nextTypescript,
